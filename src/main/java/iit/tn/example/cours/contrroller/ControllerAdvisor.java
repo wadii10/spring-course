f60b3a1909d5a1f7aa55a8f1fcb9b88afc585d3d
wadii10 <wadii.driss@gmail.com>
2025-01-22 11:55:36 +0100

    second

--- src/main/java/iit/tn/example/cours/contrroller/ControllerAdvisor.java
+++ src/main/java/iit/tn/example/cours/contrroller/ControllerAdvisor.java
@@ -1,12 +1,26 @@
 package iit.tn.example.cours.contrroller;
 
 import iit.tn.example.cours.exception.CompteNotFoundException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CompteNotFoundException.class)
-    public String handleCompteNotFoundException
+    public String handleCompteNotFoundException(CompteNotFoundException ex, Model model) {
+        model.addAttribute("timestamp", LocalDateTime.now());
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
+    //pour traiter les autres exceptions
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        model.addAttribute("timestamp", LocalDateTime.now());
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
 }
